249fbd4d4a302d332bc2b0b18bd44de538b299cf
rahul28gitfile <128899761+rahul28gitfile@users.noreply.github.com>
2024-05-28 00:32:15 +0530

    Update StaticValues.java

--- CashfreeNewProject/src/main/java/com/CashfreeNewProject/CashfreeNewProject/util/StaticValues.java
+++ CashfreeNewProject/src/main/java/com/CashfreeNewProject/CashfreeNewProject/util/StaticValues.java
@@ -11,10 +11,9 @@ package com.CashfreeNewProject.CashfreeNewProject.util;
 }
 */
 public class StaticValues {
-public static String customer_name="rahul";
-public static String customer_email="rahul@gmail.com";
-public static String customer_phone="8826572848";
-public static String customer_id="rahul123";
+
+
 public static String Client_Secret="cfsk_ma_test_94e04d43a45744f3f066d52fec1fefe*******";
 public static String Client_Id= "TEST10100568a5da89542240efed5fd48*********";
-public static String Sandbox_Url="https://sandbox.cashfree.com/pg/orders";}
+public static String Sandbox_Url="https://sandbox.cashfree.com/pg/orders";
+}
